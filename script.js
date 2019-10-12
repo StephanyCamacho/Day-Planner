1619e74e33c6f864938f8ed001321eb5240e4365
Stephany Camacho-Lara <stephany_c19@yahoo.com>
2019-10-12 17:25:37 -0600

    local storage responding correctly

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
+function getLocalStorage(key) {
+    let value = localStorage.getItem(key);
+    if (value) {
+        $(`#text${key}`).text(value);
+    }
+}
+
 $( document ).ready(function() {
     $("#currentDay").text(moment().format("MMM Do YY"));
 
+    // we have to assume that first time the page opens, nothing is saved to localStorage
+    
+        
     for (let i = 0; i < 8; i++) {
 
         // create a row
@@ -10,10 +20,10 @@ $( document ).ready(function() {
         var col1 = $('<div class="col-md-2"><p class="hour">');
 
         //create column 2
-        var col2 = $('<div class="col-md-8"><textarea class="description" placeholder="Add your event here..."></textarea>');        
+        var col2 = $(`<div class="col-md-8"><textarea id=text${i} class="description" placeholder="Add your event here..."></textarea>`);        
        
         //create column 3
-        var col3 = $('<div class="col-md-2"><button class="saveBtn">Save</button>')
+        var col3 = $(`<div class="col-md-2"><button class="saveBtn" id=${i}>Save</button>`)
         
         // append col to row
         row.append(col1);
@@ -23,10 +33,21 @@ $( document ).ready(function() {
         // last step add rows to container
         $(".container").append(row);
 
+        getLocalStorage(i);
     }
 
-    var inputText = $(".description");
     var saveBtn = $('.saveBtn');
+    saveBtn.on('click', function(){
+        let eventId = $(this).attr('id');
+        let eventText = $(this).parent().siblings().children('.description').val();
+        localStorage.setItem(eventId, eventText);
+    });
+    /*
+
+    key - the id number
+    value - text
+
+    */
 
     inputText.textContent = localStorage.getItem('');
     console.log(inputText);
@@ -41,3 +62,28 @@ $( document ).ready(function() {
         <div class="col-md-2">
           <button class="saveBtn">Save</button>
         </div> */}
+
+
+
+
+// const person = {};
+// Set a property "name" with value "Stephany" on object
+// person.name = "Stephany";
+// person['name'] = "Stephany"; // defensive programming
+// console.log(person);
+// {name: 'stephany'};
+
+// localStorage is the name of the localStorage object
+// .setItem();
+// .getItem();
+// objectName.method(key, value)
+// localStorage.setItem('name', 'Stephany');
+// localStorage.getItem('name');
+
+// let animals = ['dog','cat'];
+// // Save animals array to locaStorage. Key will be name of array.
+// // Remember you can only save strings to localStorage
+// localStorage.setItem('animals', JSON.stringify(animals));
+// let rVal = localStroage.getItem('animals'); // returns the value which is a stringified array
+// // What do we do with the reutrn value to turn it back into an array?
+// JSON.parse(rVal) // this returns the original array
